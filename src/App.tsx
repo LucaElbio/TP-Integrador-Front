5c2e1fc17226fc0c913ff80aeeb3bb5285b8f2ca
FabriQuinteros <fabricio.quinteros@smartfense.com>
2024-06-06 19:44:38 -0300

    add dependencies mui-material

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { FormControl, InputLabel, MenuItem, Select, SelectChangeEvent } from '@mui/material';
 
 function App() {
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value as string);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +17,20 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={age}
+          label="Age"
+          onChange={handleChange}
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +40,8 @@ function App() {
           Learn React
         </a>
       </header>
+
+
     </div>
   );
 }
